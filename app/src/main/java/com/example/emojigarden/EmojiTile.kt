57ab0cf7032fa18e125740e4b9c92483cd4a55cc
Aniket Kadam <aniket.kadam@mongodb.com>
2020-12-23 16:36:36 +0530

    Create a holder for the emoji

---
+++ app/src/main/java/com/example/emojigarden/EmojiTile.kt
@@ -0,0 +1,17 @@
+package com.example.emojigarden
+
+import io.realm.RealmModel
+import io.realm.RealmObject
+import io.realm.annotations.PrimaryKey
+import io.realm.annotations.RealmClass
+import org.bson.types.ObjectId
+
+@RealmClass
+open
+class EmojiTile : RealmModel {
+    @PrimaryKey
+    var _id : ObjectId = ObjectId.get()
+    var index = 0
+    var emoji : String = ""
+    var organization : String = "default" // This will be the partition key (separates synced realms so different organizations can have different gardens)
+}
